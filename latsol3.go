df0f1e1e17650caa14bfbca770e04e075d255105
Feros999 <ferospedrosa@gmail.com>
2024-10-27 08:17:44 +0700

    latsol3.go

--- latsol3.go
+++ latsol3.go
@@ -6,13 +6,8 @@ func main() {
 	var r, luas float64
 	const phi = 3.14
 
-	// Meminta input jari-jari lingkaran dari pengguna
 	fmt.Print("Masukkan jari-jari lingkaran: ")
 	fmt.Scanln(&r)
-
-	// Menghitung luas lingkaran menggunakan rumus π * r^2
 	luas = phi * r * r
-
-	// Menampilkan hasil perhitungan luas lingkaran
 	fmt.Printf("Luas lingkaran dengan jari-jari %.2f adalah %.2f\n", r, luas)
 }
